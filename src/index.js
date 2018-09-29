049237133c444675bd4bc06e8ef14bf4c987f73e
Arkadz <penkratpavel@gmail.com>
2018-09-29 10:15:43 +0300

    changed function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency>10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+        } else if (currency<=0) return {};
+    
+    let obj = {"H":0,"Q":0,"D":0,"N":0,"P":0};
+        
+    obj.H = calculate (currency, 50);
+    currency -= 50*obj.H;
+    if (obj.H == 0) delete obj.H;
+    obj.Q = calculate (currency ,25);
+    currency -= 25*obj.Q;
+    if (obj.Q == 0) delete obj.Q;
+    obj.D = calculate (currency, 10);
+    currency -= 10*obj.D;
+    if (obj.D == 0) delete obj.D;
+    obj.N = calculate (currency ,5);
+    currency -= 5*obj.N;
+    if (obj.N == 0) delete obj.N;
+    obj.P = calculate (currency ,1);
+    if (obj.P == 0) delete obj.P;
+    
+    
+    return obj;
+}
+
+const calculate = (exchange, divider) => {
+    let x = Math.floor(exchange/divider);
+    return x;
+
 }
